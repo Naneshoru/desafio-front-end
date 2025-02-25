8e708776f7983b16c481390cd8febffcb99c47fa
Ricardo Atakiama <ricardo_cssg@hotmail.com>
2025-02-25 17:57:03 -0300

    feat: formatação das datas e telefones

--- src/components/table-employees.tsx
+++ src/components/table-employees.tsx
@@ -1,14 +1,15 @@
 import React, { useCallback, useContext } from 'react';
 
-import './table-employees.css'
-import '../styles/table.css'
+import './table-employees.css';
+import '../styles/table.css';
 
 import Table from '../components/table';
 import EmployeesContext from '../contexts/employees-context';
 import { Employee } from '../models/employee';
+import { isoToDDMMYYYY, phoneFormat } from '../utils/formatters';
 
 export default function TableEmployees() {
-  const { employees } = useContext(EmployeesContext)
+  const { employees } = useContext(EmployeesContext);
 
   const fields: Array<{ name: keyof Employee, displayName: string }> = [
     { name: 'image', displayName: 'Foto' }, 
@@ -16,34 +17,33 @@ export default function TableEmployees() {
     { name: 'job', displayName: 'Cargo' }, 
     { name: 'admission_date', displayName: 'Data de admissão' },
     { name: 'phone', displayName: 'Telefone' }
-  ]
+  ];
   const mainFields: Array<keyof Employee> = ['image', 'name'];
 
   const customRows = useCallback((index: number) => {
-    if (employees?.[index] == null) return []
-    const employDaVez = employees[index]
+    if (employees?.[index] == null) return [];
+    const currentEmployee = employees[index];
     const templates = [
-      <td>{<img src={employDaVez?.image} alt="employee"></img>}</td>,
-      <td>{employDaVez.name}</td>,
-      <td>{employDaVez.job}</td>,
-      <td>{employDaVez.admission_date}</td>,
-      <td>{employDaVez.phone}</td>
+      <td>{<img src={currentEmployee?.image} alt="employee" ></img>}</td>,
+      <td>{currentEmployee.name}</td>,
+      <td>{currentEmployee.job}</td>,
+      <td>{isoToDDMMYYYY(currentEmployee.admission_date)}</td>,
+      <td>{phoneFormat(currentEmployee.phone)}</td>
     ]
     return <tr>{templates}</tr>
   }, [employees])
 
   if (employees == null) return <></>
 
-
   return (
     <>
     {/* <Debug value={customRows} /> */}
-    <Table 
-      items={employees} 
-      fields={fields} 
-      mainFields={mainFields} 
-      customRows={customRows}
-    />
+      <Table 
+        items={employees} 
+        fields={fields} 
+        mainFields={mainFields} 
+        customRows={customRows}
+      />
     </>
   );
 }
---
+++ src/utils/formatters.ts
@@ -0,0 +1,49 @@
+/**
+ * @param isoString '2019-12-02T00:00:00.000Z'
+ * @returns '11/02/2020'
+ * (no Brazil)
+ */
+const isoToDDMMYYYY = (isoString: string) => {
+  const date = new Date(isoString)
+  const year = date.getFullYear()
+  const month = date.getMonth()
+  const day = date.getDate()
+  return `${NN(day)}/${NN(month)}/${year}`
+}
+
+/**
+ * @param value '3'
+ * @returns '03'
+ */
+const NN = (value: number) => {
+  const stringValue = value.toString()
+  if (value == null || stringValue.length > 2) {
+    throw('Para resultar em dois dígitos, o número fornecido não pode ser completado com 0')
+  }
+  else if (stringValue.length === 1) {
+    return `0${stringValue}`
+  }
+
+  return value
+}
+
+const phoneFormat = (phone: string) => {
+  if (phone.length === 13) {
+    const countryCode = phone.slice(0, 2)
+    const areaCode = phone.slice(2, 4)
+    const mainPart = phone.slice(4, 9)
+    const lastPart = phone.slice(9, 13)
+    
+    return `+${countryCode} (${areaCode}) ${mainPart}-${lastPart}`
+  } else if (phone.length === 12) {
+    const countryCode = phone.slice(0, 2)
+    const areaCode = phone.slice(2, 4)
+    const mainPart = phone.slice(4, 8)
+    const lastPart = phone.slice(8, 12)
+    
+    return `+${countryCode} (${areaCode}) ${mainPart}-${lastPart}`
+  }
+  return phone
+}
+
+export { isoToDDMMYYYY, NN, phoneFormat }
