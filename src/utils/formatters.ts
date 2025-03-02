e81d849a35ebc285422b0f25f6f01b66ad56fbbb
Ricardo Atakiama <ricardo_cssg@hotmail.com>
2025-03-02 13:24:47 -0300

    fix: date.getMonth() acrescebta +1 no mês para exibir

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -6,7 +6,7 @@
 const isoToDDMMYYYY = (isoString: string) => {
   const date = new Date(isoString)
   const year = date.getFullYear()
-  const month = date.getMonth()
+  const month = date.getMonth() + 1
   const day = date.getDate()
   return `${NN(day)}/${NN(month)}/${year}`
 }
